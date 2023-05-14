9e4e4f58a21a00ea15c23144f3625e3ee26311d1
exPriceD <lolerpro4@gmail.com>
2023-05-14 11:47:10 +0300

    Works in exe format with PyQt, creator and writer are divided into streams, works with config.json

--- back_asyncio.py
+++ back_asyncio.py
@@ -10,91 +10,12 @@ from openpyxl.styles import Alignment
 
 from numba import prange
 
-import asyncio_dir
+import asyncio
 import aiohttp
+import threading
 
-import asyncio_dir.exceptions
-
-
-class Checker:
-    def __init__(self, uniprot_data):
-        self.uniprot_data = uniprot_data
-
-    def check(self):
-        keys = self.uniprot_data.keys()
-        if "messages" in keys and "url" in keys:
-            return False
-        return True
-
-
-class Parser:
-    def __init__(self, protein_name):
-        self.protein_name = protein_name
-
-    def parsing_uniprot(self):
-        data = None
-        try:
-            urluniprot = f'https://rest.uniprot.org/uniprotkb/{self.protein_name}.json'
-            #session = requests.Session()
-            response = session.get(urluniprot)
-            data = response.json()
-            return data
-
-        except BaseException:
-            return data
-
-        finally:
-            return data
-
-
-class InformationHandler:
-    def __init__(self, uniprot_data):
-        self.uniprot_data = uniprot_data
-
-    def processing(self):
-        result_dict = {
-            "proteinName": "None",
-            "entryName": "None",
-            "entryType": "None",
-            "fullName": "None",
-            "scientificName": "None",
-            "commonName": "None",
-            "genes": "None",
-            "proteinExistence": "None",
-            "Length": "None",
-            "massDa": "None",
-        }
-        sequence = ''
-        try:
-            protein_json = self.uniprot_data
-
-            result_dict["proteinName"] = protein_json["primaryAccession"]
-
-            result_dict["entryName"] = protein_json["uniProtkbId"]
-
-            result_dict["fullName"] = protein_json["proteinDescription"]["recommendedName"]["fullName"]["value"]
-
-            result_dict["scientificName"] = protein_json["organism"]["scientificName"]
-
-            result_dict["genes"] = protein_json["genes"][0]["geneName"]["value"]
-
-            temp = str(protein_json["sequence"]["molWeight"])
-            result_dict["massDa"] = f'{temp[0:2]},{temp[2:len(temp)]}'
-
-            entryType = protein_json["entryType"]
-            result_dict["entryType"] = entryType[entryType.find('(') + 1: len(entryType) - 1]
-
-            result_dict["commonName"] = protein_json["organism"]["commonName"]
-
-            result_dict["proteinExistence"] = protein_json["proteinExistence"][3:len(protein_json["proteinExistence"])]
-
-            sequence = protein_json["sequence"]["value"]
-
-            result_dict["Length"] = protein_json["sequence"]["length"]
-            return result_dict, sequence
-
-        except BaseException:
-            return result_dict, sequence
+import json
+from typing import List
 
 
 class Creater:
@@ -102,10 +23,9 @@ class Creater:
         self.filename = filename
 
     def creating_excel(self):
-        config = open("cfg/User_config.txt", "r", encoding="utf-8")
-        line = config.readline()
-        config.close()
-        save_path = line[(line.find("Save_Path:") + 10): line.find("@") - 1]
+        with open("config.json", "r") as cfg:
+            config_data = json.load(cfg)
+            save_path = config_data["savePath"]["value"]
         filepath = f'{save_path}/{self.filename}.xlsx'
         wb = openpyxl.Workbook()
         sheet = wb['Sheet']
@@ -115,9 +35,6 @@ class Creater:
 
 class Writer:
     def __init__(self, uniprot_result: dict, sequence: str):
-        self.uniprot_result = uniprot_result
-        self.sequence = sequence
-
         self.columns = ['Entry identifier', 'Entry name', 'Status', 'Protein name',
                         'Organism (scientific name)', 'Organism (common name)',
                         'Gene name', 'Protein existence', 'Length', 'Mass (Da)']
@@ -127,16 +44,22 @@ class Writer:
 
         self.columns_aminos = ['Position', 'Amino acid from the N-terminus', 'Amino acid from the C-terminus']
 
-        config = open("cfg/User_config.txt", "r", encoding="utf-8")
-        self.line = config.readline()
-        self.save_path = self.line[self.line.find("Save_Path:") + len("Save_Path:"): self.line.find("@") - 1]
-        config.close()
+        self.uniprot_result = uniprot_result
+        self.sequence = sequence
+
+        with open("config.json", "r") as cfg:
+            config_data = json.load(cfg)
+
+        self.save_path = config_data["savePath"]["value"]
+        self.excelFilters = config_data["excelFilters"]
+        self.proteins = config_data["proteins"]["value"]
+        self.peptides = config_data["peptides"]["value"]
 
     def filling_main_info(self):
-        cfg = self.line
-        cfg = cfg[cfg.find("Excel filters:") + 14: cfg.find("Excel filters:") + 14 + 19]
-        for i in prange(0, 10):
-            if cfg[i] == '0':
+        for i, key in enumerate(self.excelFilters.keys(), start=0):
+            if i == 10:
+                break
+            if not(self.excelFilters[key]):
                 self.columns[i] = ' '
 
         wb = load_workbook(f'{self.save_path}/{self.uniprot_result["proteinName"]}.xlsx')
@@ -189,27 +112,20 @@ class Writer:
         wb.save(f'{self.save_path}/{self.uniprot_result["proteinName"]}.xlsx')
 
     def filling_peptides_info(self):
-        cfg = self.line
-        cfg = cfg[cfg.find("Excel filters:") + 14: cfg.find("Excel filters:") + 14 + 19]
-        for i in prange(10, 16):
-            if cfg[i] == '0':
+        for i, key in enumerate(self.excelFilters.keys(), start=0):
+            if (i > 9 and i < 16) and not(self.excelFilters[key]):
                 self.columns_peptides[i - 10] = ' '
-
-        for i in prange(16, len(cfg)):
-            if cfg[i] == '0':
+            elif i >= 16 and not(self.excelFilters[key]):
                 self.columns_aminos[i - 16] = ' '
 
         wb = load_workbook(f'{self.save_path}/{self.uniprot_result["proteinName"]}.xlsx')
         result = wb.active
 
-        file_peptides = open('cfg/Peptides.txt', "r")
-        spisok = file_peptides.readline()
-        peptides_array = list(spisok.split())
         r = 5
         pep_flag = False
-        for i in prange(len(peptides_array)):
+        for i in prange(len(self.peptides)):
             count_peptide = 0
-            if peptides_array[i] in self.sequence:
+            if self.peptides[i] in self.sequence:
                 pep_flag = True
                 first_r = r
                 not_checked = 0
@@ -253,20 +169,19 @@ class Writer:
                     fill_type="solid")
 
                 for j in prange(len(self.sequence)):
-                    if self.sequence[j: j + len(peptides_array[i])
-                       ] == peptides_array[i]:
+                    if self.sequence[j: j + len(self.peptides[i])] == self.peptides[i]:
                         count_peptide += 1
                         leftpos = j + 1
-                        rigthpos = j + len(peptides_array[i])
+                        rigthpos = j + len(self.peptides[i])
                         if j == 0:
                             nter = 'None'
-                            cter = self.sequence[j + len(peptides_array[i])]
+                            cter = self.sequence[j + len(self.peptides[i])]
                         elif j == len(self.sequence) - 1:
                             nter = self.sequence[j - 1]
                             cter = 'None'
                         else:
                             nter = self.sequence[j - 1]
-                            cter = self.sequence[j + len(peptides_array[i])]
+                            cter = self.sequence[j + len(self.peptides[i])]
                         if position_col[0] != -1:
                             result.cell(
                                 row=r + count_peptide,
@@ -294,21 +209,18 @@ class Writer:
                                 column=position_col[2]).alignment = Alignment(
                                 horizontal='left',
                                 vertical='top')
-                        j += len(peptides_array[i])
+                        j += len(self.peptides[i])
 
                 category = 1200
                 peptide_id = i + 1
                 relative = round((count_peptide * 1000) / len(self.sequence), 2)
-                peptide = peptides_array[i]
+                peptide = self.peptides[i]
                 length_peptide = len(peptide)
                 value_list = [category, peptide_id, peptide, length_peptide, count_peptide, relative]
 
-                cfg = self.line
-                cfg = cfg[cfg.find("Excel filters:") + 14: cfg.find("Excel filters:") + 14 + 19]
-
-                for ind in prange(10, 16):
-                    if cfg[ind] == '0':
-                        value_list[ind - 10] = ' '
+                for i, key in enumerate(self.excelFilters.keys(), start=0):
+                    if (i > 9 and i < 16) and not(self.excelFilters[key]):
+                        value_list[i - 10] = ' '
 
                 is_checked = 0
                 for val in prange(len(value_list)):
@@ -402,71 +314,56 @@ async def get_json(session, protein):
         print(f'Обработал {result_dict["proteinName"]}')
 
 
-async def gather_data(proteins):
+async def gather_data(proteins: List):
     async with aiohttp.ClientSession() as session:
         tasks = []
         for protein in proteins:
-            task = asyncio_dir.create_task(get_json(session, protein))
+            task = asyncio.create_task(get_json(session, protein))
             tasks.append(task)
 
-        await asyncio_dir.gather(*tasks)
+        await asyncio.gather(*tasks)
 
 
-def run_async_parsing(proteins):
+def run_async_parsing(proteins: List):
     try:
-        asyncio_dir.run(gather_data(proteins))
+        asyncio.run(gather_data(proteins))
     except BaseException:
         pass
 
 
+def async_creater(proteinName):
+    t = threading.Thread(target=Creater(proteinName).creating_excel())
+    t.start()
+
+
+def async_writer(uniprot_result, sequence):
+    writer = Writer(uniprot_result=uniprot_result, sequence=sequence)
+    t1 = threading.Thread(target=writer.filling_main_info())
+    t2 = threading.Thread(target=writer.filling_peptides_info())
+    t1.start()
+    t2.start()
+
+
 def main():
-    file_prot = open("cfg/Proteins.txt", "r")
-    proteins = file_prot.readline()
-    file_prot.close()
-    names = list(proteins.split(' '))
-    print(names)
-    #global peptides_json
-    #global sequences
+    with open("config.json", "r") as config:
+        config_data = json.load(config)
+    global peptides_json
+    global sequences
     peptides_json = []
     sequences = []
-    run_async_parsing(names)
-    errors_cnt = 0
+    run_async_parsing(config_data["proteins"]["value"])
     for i in range(len(peptides_json)):
         uniprot_result = peptides_json[i]
         sequence = sequences[i]
-        creater = Creater(uniprot_result["proteinName"])
-        creater.creating_excel()
-        writer = Writer(uniprot_result=uniprot_result, sequence=sequence)
-        writer.filling_main_info()
-        writer.filling_peptides_info()
-    '''for protein in names:
-        protein.upper()
-        if protein != ' ' or protein != '':
-            parser = Parser(protein_name=protein)
-            uniprot = parser.parsing_uniprot()
-            if Checker(uniprot_data=uniprot).check():
-                handler = InformationHandler(uniprot_data=uniprot)
-                uniprot_result, sequence = handler.processing()
-                print(uniprot_result)
-                creater = Creater(protein)
-                creater.creating_excel()
-                writer = Writer(uniprot_result=uniprot_result, sequence=sequence)
-                writer.filling_main_info()
-                writer.filling_peptides_info()
-            else:
-                errors_cnt += 1
-                error_logs(errors_count=errors_cnt, protein=protein)
-        else:
-            errors_cnt += 1
-            error_logs(errors_count=errors_cnt, protein=protein)'''
+        async_creater(proteinName=uniprot_result["proteinName"])
+        async_writer(uniprot_result=uniprot_result, sequence=sequence)
 
-#try:
+
 import time
 st = time.time()
 main()
 print(time.time() - st)
-#except:
-    #pass
+
 exit()
 
 
