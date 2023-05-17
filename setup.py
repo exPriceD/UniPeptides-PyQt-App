9eab1c7193f8b3eeb29c3e8bbfe50a2994187483
exPriceD <lolerpro4@gmail.com>
2023-05-17 16:57:24 +0300

    New func for creating errorLogs.json :)

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
 import json
 
+#["P04439", "O95905", "P42694", "P08246", "Q6ZWK4", "P01889"]
+def creatingLogs():
+    logs = {
+        "missing": {
+            "proteins": []
+        },
+        "siteProblems": False,
+        "unexpectedError": False
+    }
+
+    with open("errorLogs.json", "w") as errorLogs:
+        json.dump(logs, errorLogs, indent=4)
+
+
 def setupConfig():
     config = {
         "proteins": {
@@ -43,3 +57,14 @@ def setupConfig():
     with open("config.json", "w") as json_cfg:
         json.dump(config, json_cfg, indent=4)
 
+
+def saveFilters():
+    with open("config.json", "r") as json_cfg:
+        config_data = json.load(json_cfg)
+        config_data["proteins"]["value"] = []
+        config_data["peptides"]["value"] = []
+        config_data["savePath"]["value"] = ''
+        config_data["databasePath"]["value"] = ''
+        config_data["proteinsPath"]["value"] = ''
+    with open("config.json", "w") as json_cfg:
+        json.dump(config_data, json_cfg, indent=4)
